4c6c832d249689d19fac692ee7ebfc9a2bea124f
Matheus Serpellone <m.serpellone@gmail.com>
2014-10-09 10:55:13 +0100

    further clauses to prolog file

--- PSA/lab0/family.pl
+++ PSA/lab0/family.pl
@@ -1,5 +1,24 @@
 % Family relationships (after Bratko (2001), program 1.8)
 
+parent(pam,bob).
+parent(tom,bob).
+parent(tom,liz).
+parent(bob,ann).
+parent(bob,pat).
+parent(pat,jim).
+parent(liz,jim).
+parent(ed,jim).
+
+male(tom).     
+male(bob).
+male(jim).
+female(pam).
+female(liz).
+female(ann).
+female(pat).
+
+
+
 father(X,Y) :-
     parent(X,Y),
     male(X).
@@ -12,12 +31,13 @@ grandparent(X,Z) :-
     parent(X,Y),
     parent(Y,Z).
 
-% X is Y's sibling
+
 sibling(X,Y) :-
 	parent(X,Z),
-	parent(Y,Z).
-	
-% X is Y's sister
+	parent(Y,Z),
+	X \= Y.
+
+% X is Y's sister	
 sister(X,Y) :-
 	sibling(X,Y),
 	female(X).
@@ -26,20 +46,7 @@ sister(X,Y) :-
 brother(X,Y) :-
 	sibling(X,Y),
 	male(X).
+
+% X is Y's uncle or aunt
+tios(X,Y) :-
 	
-parent(pam,bob).
-parent(tom,bob).
-parent(tom,liz).
-parent(bob,ann).
-parent(bob,pat).
-parent(pat,jim).
-parent(liz,jim).
-
-male(tom).     
-male(bob).
-male(jim).
-female(pam).
-female(liz).
-female(ann).
-female(pat).
-
