e7d61a356ca2d6c5c37a528be1ca1d1b85ef3498
Matheus <m.serpellone@gmail.com>
2014-11-12 16:27:57 +0000

    stable version of skin detection

--- IIP/matlab/coursework_probability_based/process_test.m
+++ IIP/matlab/coursework_probability_based/process_test.m
@@ -1,15 +1,18 @@
 function y=process_test()
+
     files = dir('test/');
     
     accuHist = zeros(256,256,256);
     pixSum = 0;
     
+    files=[struct('name','test2.jpg', 'isdir',false)];
     %for each image
     for file = files'
         if(file.isdir) 
             continue; 
         end
         image = imread(strcat('test/',file.name));
+        
         
         %calculate its histogram
         hist = imColourHist(image);
@@ -21,6 +24,7 @@ function y=process_test()
     
     %normalise histogram (probability)
     y=accuHist/pixSum;
+    
 end
 
 
@@ -30,8 +34,17 @@ function y=imColourHist(img)
     
     for i=1:size(img,1)
         for j=1:size(img,2)
-            currVal = hist(img(i,j,1)+1,img(i,j,2)+1,img(i,j,3)+1);
-            hist(img(i,j,1)+1,img(i,j,2)+1,img(i,j,3)+1) = currVal + 1 ;
+            red = img(i,j,1);
+            green = img(i,j,2);
+            blue = img(i,j,3);
+            
+            %ignore really dark colours
+            if(red<20 && green<20 && blue<20)
+                continue;
+            end
+            
+            currVal = hist(red+1,green+1,blue+1);
+            hist(red+1,green+1,blue+1) = currVal + 1 ;
         end
     end
     
--- IIP/matlab/coursework_probability_based/skin_detect.m
+++ IIP/matlab/coursework_probability_based/skin_detect.m
@@ -1,16 +1,40 @@
 function skin_detect(filename)
-    limit=0.0001;
     img = imread(filename);
+    
+    %sees training set and returns a probability density matrix
     pHist = process_test();
+    
+    %test image appears to be noisy (there are high values even after
+    %applying median filter)
+    %So we need to decide a limit... takes the median of all non-zero
+    %values and multiply by 10
+    limit = median(pHist(pHist~=0));
+    
+    %Now compares the image. Checks for each pixel what's the probability
+    %of it being a skin pixel.
     mask = zeros(size(img,1),size(img,2),3);
     for i=1:size(img,1)
         for j=1:size(img,2)
             pSkin = isSkin(img(i,j,:), pHist);
-            if(pSkin<limit)
+            if(pSkin>limit)
                 mask(i,j,1)=255;
             end
         end
     end
+    
+    %Now we have a mask, but it probably is very noisy.
+    %So let's apply a dilation/erosion technique so we get rid of small
+    %noise that might appear on non-skin pixels.
+    %Since small values for the structuring element might not be effective,
+    %large values might destroy the mask on smalled images. So let's
+    %calculate the size of the structuring element as being a fraction of
+    %the image's dimension (larger images should have larger structuring
+    %elements)
+    radius = round(size(img,1)*size(img,2)/30000);
+    mask(:,:,1) = imdilate(mask(:,:,1), strel('diamond',radius) );
+    mask(:,:,1) = imerode (mask(:,:,1), strel('diamond',radius) );
+    
+    
     subplot(1,2,1),imshow(mask);
     subplot(1,2,2),imshow(img);
 end
